e5f39ac9ae7c0321ae1b80daac1b8dfa14d528ac
Nikolai <kolyanterek@bk.ru>
2024-04-01 18:35:05 +0300

    Переделал считывание key3. Теперь случай с : тоже отрабатывает корректно

--- teretshenkov.nikolay/T2/DataStruct.h
+++ teretshenkov.nikolay/T2/DataStruct.h
@@ -115,10 +115,10 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
     for (int i = 0; i < 3; ++i)
     {
         std::getline(in, nKey, ' ');
-        std::getline(in, value, ':');
         std::istringstream valueStream(value);
         if (nKey == "key1")
         {
+            std::getline(in, value, ':');
             if (isCorrectDouble(value))
             {
                 valueStream >> dest.key1;
@@ -130,6 +130,7 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
         }
         else if (nKey == "key2")
         {
+            std::getline(in, value, ':');
             if (isCorrectULL(value))
             {
                 valueStream >> std::hex >> dest.key2;
@@ -141,7 +142,10 @@ std::istream& operator>>(std::istream& in, DataStruct& dest)
         }
         else if (nKey == "key3")
         {
-            dest.key3 = value;
+            in.ignore(1);
+            std::getline(in, value, '"');
+            dest.key3 = (value + std::string("\""));
+            in.ignore(1);
         }
     }
     in.ignore(1);
