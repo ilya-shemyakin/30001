96c6eb6c0e5ea769e50d30711832be51f340710d
EdwardShiroki <164063359+EdwardShiroki@users.noreply.github.com>
2024-05-11 01:09:48 +0300

    исправлено сообщение об ошибке

--- melnikov.vladimir/T3/main.cpp
+++ melnikov.vladimir/T3/main.cpp
@@ -57,7 +57,7 @@ int main(int argc, char** argv) {
         }
         catch (...)
         {
-            std::cout << "INVALID COMMAND";
+            std::cout << "<INVALID COMMAND>" << '\n';
         }
     }
 
