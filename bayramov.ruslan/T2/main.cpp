8e8ee89c888a0dd1af17305ba24bb3f982d67faa
Ruslan_The_Programmer <brusl426@gmail.com>
2024-05-15 10:20:52 +0300

    bayramov.ruslan/T2

---
+++ bayramov.ruslan/T2/main.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <cassert>
+#include <iterator>
+#include <vector>
+#include <iomanip>
+#include <algorithm>
+#include "DataStruct.h"
+
+
+using std::cout;
+using std::endl;
+
+
+int main()
+{
+    using namespace nspace;
+
+    setlocale(LC_ALL, "rus");
+    std::vector<DataStruct> data;
+
+    while (std::cin.good())
+    {
+        std::copy(
+            std::istream_iterator< DataStruct >(std::cin),
+            std::istream_iterator< DataStruct >(),
+            std::back_inserter(data)
+        );
+        if (!std::cin.eof() && std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore();
+        }
+    }
+
+
+    std::sort(data.begin(), data.end(), [](const DataStruct& first, const DataStruct& second)
+        {
+            if (first.key1 != second.key1)
+            {
+                return second.key1 > first.key1;
+            }
+            if (first.key2 != second.key2)
+            {
+                return second.key2 > first.key2;
+            }
+            return second.key3.length() > first.key3.length();
+        }
+    );
+    
+    
+
+    std::copy(
+        std::begin(data),
+        std::end(data),
+        std::ostream_iterator< DataStruct >(std::cout, "\n")
+    );
+    return 0;
+}
