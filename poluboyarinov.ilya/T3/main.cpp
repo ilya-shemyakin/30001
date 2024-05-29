58edf2b47081ebf046d2e990fc0066ba7a809279
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2024-05-30 01:34:20 +0300

    Update main.cpp

--- poluboyarinov.ilya/T3/main.cpp
+++ poluboyarinov.ilya/T3/main.cpp
@@ -2,37 +2,6 @@
 #include <fstream>
 #include "Polygon.h"
 #include "PolygonFunctions.h"
-
-/*
-3 (0;0) (0;2) (2;0)
-4 (0;1) (1;1) (1;0) (0;0)
-5 (0;0) (1;0) (2;1) (1;2) (0;2)
-3 (4;3) (4;2) (3;2)
-4 (0;0) (1;0) (1;1) (0;1)
-3 (0;0) (1;1) (0;1)
-4 (1;1) (2;1) (2;2) (1;2)
-SAME 4 (-1;-1) (-1;0) (0;0) (0;-1)
-SAME 3 (10;10) (11;11) (10;11)
-SAME 3 (10;10) (10;11) (11;10)
-7 (0;0) (0;4) (2;6) (4;6) (6;4) (6;2) (4;0)
-7 (1;1) (1;5) (3;7) (5;7) (7;5) (7;3) (5;1)
-7 (-1;-1) (-1;3) (1;5) (3;5) (5;3) (5;1) (3;-1)
-4 (2;2) (3;2) (3;3) (2;3)
-SAME 7 (0;0) (0;4) (2;6) (4;6) (6;4) (6;2) (4;0)
-SAME 4 (0;0) (1;0) (1;1) (0;1)
-SAME 4 (1;1) (2;1) (3;3) (1;2)
-SAME 2 (5;5) (5;6)
-SAME 4 (5;5) (6;5) (6;6) (;)
-SAME 3 (5;5) (6;5) (6;6) (6;6)
-SAME 3 (5;5) (6;5)
-
-3 (0;0) (2;0) (0;2)
-8 (0;2) (0;4) (2;6) (4;6) (6;4) (6;2) (4;0) (2;0)
-3 (0;0) (2;0) (0;2)
-
-cd source\repos\T3\x64\Debug
-*/
-
 int main(int countOfArg, char** name)
 {
     std::cout << "hello\n";
@@ -40,29 +9,12 @@ int main(int countOfArg, char** name)
     {
         return 1;
     }
-
     std::ifstream inpFile(name[1]);
     if (!inpFile)
     {
         return 1;
     }
-
     std::vector<nspace::Polygon> polygons;
-
-    /*while (std::cin.good())
-    {
-        std::copy(
-            std::istream_iterator<nspace::Polygon>(std::cin),
-            std::istream_iterator<nspace::Polygon>(),
-            std::back_inserter(polygons)
-        );
-        if (std::cin.fail() && !std::cin.eof())
-        {
-            std::cin.clear();
-            std::cin.ignore();
-        }
-    }*/
-
     while (inpFile.good())
     {
         std::copy(
@@ -76,11 +28,8 @@ int main(int countOfArg, char** name)
             inpFile.ignore();
         }
     }
-
     std::cout << polygons.size() << "\n";
-
     std::cin.clear();
-
     std::string command;
     while (!std::cin.eof())
     {
@@ -89,7 +38,6 @@ int main(int countOfArg, char** name)
             std::cin.clear();
             std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
-
         try
         {
             std::cin >> command;
@@ -132,6 +80,5 @@ int main(int countOfArg, char** name)
             std::cout << "<INVALID COMMAND>" << '\n';
         }
     }
-    
     return 0;
 }
