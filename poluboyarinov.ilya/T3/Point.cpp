b3f3033dde6c85923cba56fe389e2ea8295bc333
IghorTatar <84169804+IghorTatar@users.noreply.github.com>
2024-05-30 01:31:47 +0300

    Update Point.cpp

--- poluboyarinov.ilya/T3/Point.cpp
+++ poluboyarinov.ilya/T3/Point.cpp
@@ -1,6 +1,5 @@
 #include "Point.h"
-
 bool nspace::operator==(const Point& first, const Point& second)
 {
-    return first.x == second.x && first.y == second.y;
+    return (first.x == second.x && first.y == second.y);
 }
