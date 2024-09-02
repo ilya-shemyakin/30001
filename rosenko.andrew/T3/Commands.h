7be9052b7f63ce832cd3cb8bb895cef4d59859dc
lorette-m <lory_works@outlook.com>
2024-09-02 21:30:47 +0300

    Fix the incorrect format error 4 (turns all files in CR)

--- rosenko.andrew/T3/Commands.h
+++ rosenko.andrew/T3/Commands.h
@@ -1,32 +1,1 @@
-#ifndef COMMANDS_H
-#define COMMANDS_H
-
-#include <functional>
-#include <iomanip>
-#include <algorithm>
-#include <numeric>
-#include <cmath>
-#include <string>
-
-#include "Polygon.h"
-
-bool isEven(const Polygon& polygon);
-bool isDigit(char ch);
-double calcArea(const Polygon& polygon);
-
-void area(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-void max(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-void min(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-void count(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-void rmecho(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-
-int getMaxCoordinate(const Polygon& polygon, int(*getCoordinate)(const Point& point));
-int getMinCoordinate(const Polygon& polygon, int(*getCoordinate)(const Point& point));
-int getX(const Point& point);
-int getY(const Point& point);
-std::vector< Point > getFrame(const std::vector< Polygon >& polygons);
-bool isInFrame(std::vector< Point >& frame, const Polygon& polygon);
-
-void inframe(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);
-
-#endif
+#ifndef COMMANDS_H#define COMMANDS_H#include <functional>#include <iomanip>#include <algorithm>#include <numeric>#include <cmath>#include <string>#include "Polygon.h"bool isEven(const Polygon& polygon);bool isDigit(char ch);double calcArea(const Polygon& polygon);void area(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);void max(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);void min(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);void count(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);void rmecho(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);int getMaxCoordinate(const Polygon& polygon, int(*getCoordinate)(const Point& point));int getMinCoordinate(const Polygon& polygon, int(*getCoordinate)(const Point& point));int getX(const Point& point);int getY(const Point& point);std::vector< Point > getFrame(const std::vector< Polygon >& polygons);bool isInFrame(std::vector< Point >& frame, const Polygon& polygon);void inframe(std::vector< Polygon >& polygons, std::istream& input, std::ostream& output);#endif
--- rosenko.andrew/T3/Polygon.h
+++ rosenko.andrew/T3/Polygon.h
@@ -1,99 +1,1 @@
-#ifndef POLYGON_H
-#define POLYGON_H
-
-#include <iostream>
-#include <vector>
-#include <string>
-#include <iterator>
-#include <sstream>
-
-#include "Iofmtguard.h"
-
-struct Point
-{
-    int x;
-    int y;
-
-    bool operator ==(const Point& other) const;
-};
-
-struct Polygon
-{
-    std::vector< Point > vertexes;
-
-    bool operator==(const Polygon& other) const;
-};
-
-struct DelimiterIO
-{
-    char exp;
-};
-
-struct intIO
-{
-    int& ref;
-};
-
-struct DelStrIO
-{
-    std::string exp;
-};
-
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-std::istream& operator>>(std::istream& in, intIO&& dest);
-std::istream& operator>>(std::istream& in, Point& dest);
-std::istream& operator>>(std::istream& in, Polygon& dest);
-
-std::ostream& operator<<(std::ostream& ost, const Point& point);
-std::ostream& operator<<(std::ostream& ost, const Polygon& polygon);
-
-#endif#ifndef POLYGON_H
-#define POLYGON_H
-
-#include <iostream>
-#include <vector>
-#include <string>
-#include <iterator>
-#include <sstream>
-
-#include "Iofmtguard.h"
-
-struct Point
-{
-    int x;
-    int y;
-
-    bool operator ==(const Point& other) const;
-};
-
-struct Polygon
-{
-    std::vector< Point > vertexes;
-
-    bool operator==(const Polygon& other) const;
-};
-
-struct DelimiterIO
-{
-    char exp;
-};
-
-struct intIO
-{
-    int& ref;
-};
-
-struct DelStrIO
-{
-    std::string exp;
-};
-
-std::istream& operator>>(std::istream& in, DelimiterIO&& dest);
-std::istream& operator>>(std::istream& in, intIO&& dest);
-std::istream& operator>>(std::istream& in, Point& dest);
-std::istream& operator>>(std::istream& in, Polygon& dest);
-
-std::ostream& operator<<(std::ostream& ost, const Point& point);
-std::ostream& operator<<(std::ostream& ost, const Polygon& polygon);
-
-#endif
+#ifndef POLYGON_H#define POLYGON_H#include <iostream>#include <vector>#include <string>#include <iterator>#include <sstream>#include "Iofmtguard.h"struct Point{    int x;    int y;    bool operator ==(const Point& other) const;};struct Polygon{    std::vector< Point > vertexes;    bool operator==(const Polygon& other) const;};struct DelimiterIO{    char exp;};struct intIO{    int& ref;};struct DelStrIO{    std::string exp;};std::istream& operator>>(std::istream& in, DelimiterIO&& dest);std::istream& operator>>(std::istream& in, intIO&& dest);std::istream& operator>>(std::istream& in, Point& dest);std::istream& operator>>(std::istream& in, Polygon& dest);std::ostream& operator<<(std::ostream& ost, const Point& point);std::ostream& operator<<(std::ostream& ost, const Polygon& polygon);#endif#ifndef POLYGON_H#define POLYGON_H#include <iostream>#include <vector>#include <string>#include <iterator>#include <sstream>#include "Iofmtguard.h"struct Point{    int x;    int y;    bool operator ==(const Point& other) const;};struct Polygon{    std::vector< Point > vertexes;    bool operator==(const Polygon& other) const;};struct DelimiterIO{    char exp;};struct intIO{    int& ref;};struct DelStrIO{    std::string exp;};std::istream& operator>>(std::istream& in, DelimiterIO&& dest);std::istream& operator>>(std::istream& in, intIO&& dest);std::istream& operator>>(std::istream& in, Point& dest);std::istream& operator>>(std::istream& in, Polygon& dest);std::ostream& operator<<(std::ostream& ost, const Point& point);std::ostream& operator<<(std::ostream& ost, const Polygon& polygon);#endif
