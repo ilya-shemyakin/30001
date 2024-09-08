9eb642c7f736fdce567af8654946585c875fd27f
EdwardShiroki <164063359+EdwardShiroki@users.noreply.github.com>
2024-09-08 18:28:00 +0300

    вывод структуры

--- karpov.demid/Т2/DataStruct.cpp
+++ karpov.demid/Т2/DataStruct.cpp
@@ -67,4 +67,63 @@ namespace nspace {
         return in >> DelStrIO{"(:N"} >> dest.ref.first >> DelStrIO{":D"}
                   >> dest.ref.second >> DelStrIO{":)"};
     }
+
+    std::istream &operator>>(std::istream &in, DataStruct &dest)
+    {
+        std::istream::sentry sentry(in);
+        if (!sentry)
+        {
+            return in;
+        }
+        DataStruct input;
+        {
+            using sep = DelimiterIO;
+            using sepStr = DelStrIO;
+            using chrLit = ChrLitIO;
+            using ratLSP = RatLSPIO;
+            using str = StringIO;
+            in >> sepStr({"(:"});
+            int keyNumber = 0;
+            for (size_t i = 0; i < 3 ; i++) {
+                in >> sepStr({"key"}) >> keyNumber;
+                if (keyNumber == 1)
+                {
+                    in >> chrLit{ input.key1 };
+                }
+                else if (keyNumber == 2)
+                {
+                    in >> ratLSP {input.key2};
+                }
+                else if (keyNumber == 3)
+                {
+                    in >> str{input.key3};
+                }
+                else
+                {
+                    in.setstate(std::ios::failbit);
+                }
+                in >> sep{ ':'};
+            }
+            in >> sep{ ')'};
+        }
+        if (in)
+        {
+            dest = input;
+        }
+        return in;
+    }
+
+    std::ostream &operator<<(std::ostream &out, const DataStruct &src)
+    {
+        std::ostream::sentry sentry(out);
+        if (!sentry)
+        {
+            return out;
+        }
+        iofmtguard fmtguard(out);
+        out << "(:key1 " << src.key1;
+        out << ":key2 (:N " << src.key2.first << ":D " << src.key2.second << ":)";
+        out << ":key3 \"" << src.key3 << "\":)";
+        return out;
+    }
 }
