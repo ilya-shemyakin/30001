784893d74bc115e4b0dee4d15b93ac496f8a1947
EdwardShiroki <121825965+dwdfedf123@users.noreply.github.com>
2024-09-09 10:59:25 +0300

    файлы + база структуры

--- karpov.demid/T2/DataStruct.h
+++ karpov.demid/T2/DataStruct.h
@@ -1,56 +1,42 @@
 #ifndef T2_DATASTRUCT_H
 #define T2_DATASTRUCT_H
 #include <iostream>
-namespace nspace
-{
-    struct DataStruct
-    {
+namespace nspace {
+    struct DataStruct {
         char key1;
         std::pair<long long, unsigned long long> key2;
         std::string key3;
     };
-    struct DelimiterIO
-    {
+    struct DelimiterIO {
         char exp;
     };
 
-    struct ChrLitIO
-    {
+    struct ChrLitIO {
         char &ref;
     };
 
-    struct RatLSPIO
-    {
+    struct RatLSPIO {
         std::pair<long long, unsigned long long> &ref;
     };
-    struct StringIO
-    {
+    struct StringIO {
         std::string &ref;
     };
 
-    struct DelStrIO
-    {
+    struct DelStrIO {
         std::string exp;
     };
 
-    class iofmtguard
-    {
+    class iofmtguard {
     public:
-        iofmtguard(std::basic_ios< char > &s);
+        iofmtguard(std::basic_ios<char> &s);
+
         ~iofmtguard();
+
     private:
-        std::basic_ios< char > &s_;
+        std::basic_ios<char> &s_;
         char fill_;
         std::streamsize precision_;
-        std::basic_ios< char >::fmtflags fmt_;
+        std::basic_ios<char>::fmtflags fmt_;
     };
-
-    std::istream &operator>>(std::istream &in, DelimiterIO &&dest);
-    std::istream &operator>>(std::istream &in, RatLSPIO &&dest);
-    std::istream &operator>>(std::istream &in, ChrLitIO &&dest);
-    std::istream &operator>>(std::istream &in, StringIO &&dest);
-    std::istream &operator>>(std::istream &in, DelStrIO &&dest);
-    std::istream &operator>>(std::istream &in, DataStruct &dest);
-    std::ostream &operator<<(std::ostream &out, const DataStruct &src);
 }
 #endif //T2_DATASTRUCT_H
