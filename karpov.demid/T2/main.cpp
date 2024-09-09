7f80162aa3c590da2b4e42a005deacc0483b2b2f
EdwardShiroki <121825965+dwdfedf123@users.noreply.github.com>
2024-09-09 11:03:45 +0300

    ввод в мейне

--- karpov.demid/T2/main.cpp
+++ karpov.demid/T2/main.cpp
@@ -1,4 +1,20 @@
-int main()
-{
+#include <vector>
+#include <algorithm>
+#include <iterator>
+#include "DataStruct.h"
 
+int main() {
+    using nspace::DataStruct;
+    std::vector<DataStruct> data;
+    while (std::cin.good()) {
+        std::copy(
+                std::istream_iterator<DataStruct>(std::cin),
+                std::istream_iterator<DataStruct>(),
+                std::back_inserter(data)
+        );
+        if (std::cin.fail() && !std::cin.eof()) {
+            std::cin.clear();
+            std::cin.ignore();
+        }
+    }
 }
